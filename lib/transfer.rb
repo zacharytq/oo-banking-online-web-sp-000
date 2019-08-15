3ce7cedac921b18f9ed4a003c8b360261c5f3b24
chic-page-6502 <zacharytq@gmail.com>
2019-08-15 23:00:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
       @receiver.deposit(@amount)
       @status = "complete"
     else
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
